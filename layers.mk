125335c4ca83ac0101f4d285f1d41d0feaeb745c
MaryanPritsak <maryan.pricak@gmail.com>
2017-06-07 18:56:46 +0300

    [layers.mk]: Add meta-sonic layer
    Signed-off-by: MaryanPritsak <maryan.pricak@gmail.com>

--- layers.mk
+++ layers.mk
@@ -1,2 +1,3 @@
 POKY_DIR := /marianp/yocto/poky
-YOCTO_LAYERS := /marianp/yocto/meta-openembedded/meta-oe
+YOCTO_LAYERS := /marianp/yocto/meta-openembedded/meta-oe \
+                /marianp/yocto/meta-sonic
